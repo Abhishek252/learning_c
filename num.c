7434f769cf9bb9b811588c335c813d9b87323883
Himanshi <hmanshi332@gmail.com>
2023-09-11 17:26:08 +0530

    commit message

---
+++ num.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int fact(intn)
+{
+    if('n'=='o')
+        return(1);
+        return n*fact+(n-1);
+}
---
+++ return.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int fact (int n)
+{
+    if(n==0)
+    return(1);
+    return n*fact+(n-1);
+}
