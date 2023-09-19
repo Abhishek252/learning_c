16cf68fb0fb6aa835d3a5713211dd1839506d3c1
Himanshi <hmanshi332@gmail.com>
2023-09-19 22:47:38 +0530

    commit message

---
+++ t.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int a=5,b;
+    printf("%d",a,b);
+    getch();
+}
---
+++ tutorial1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=5,b;
+    b=a--;
+    printf("%d",a,b);
+    getch();
+}
+
+
