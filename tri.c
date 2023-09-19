db0c14d879b8b080c1dff48a8b7a143301efdb88
Himanshi <hmanshi332@gmail.com>
2023-09-19 23:21:02 +0530

    commit message

--- tri.c
+++ tri.c
@@ -5,5 +5,5 @@ int main()
     int x=6;
     x%=3;
     printf("%d",x);
-    'return0';
+    getch();
 }
--- tutorial1.c
+++ tutorial1.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-    int a=5,b;
-    b=a--;
-    printf("%d",a,b);
-    getch();
+    FILE *fptr;
+    fptr=fopen("filename.txt","r");
+    if (fptr == NULL){
+        printf("The file is not opend. The program will" "now exit.");
+        exit(0);
+    }
+    'return0';
 }
 
 
