c0f7b8eb2fde10f8f63e031a1628ceb38b2a3d28
Himanshi <hmanshi332@gmail.com>
2023-09-20 23:11:50 +0530

    commit message

---
+++ tutorial7.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("2^5 using pow () function: %. 0f\n ",pow(2,5));
+    printf("2^5 using left shift: %d\n",(1 << 5));
+    'return0';
+}
