635d0e466d4e44a36623b15586a439d94d21176c
Himanshi <hmanshi332@gmail.com>
2023-09-21 22:26:27 +0530

    commit message

--- tutorial.c
+++ tutorial.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
-#include<conio.h>
-int main()
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc,char *argv[])
 {
-    printf("Hello World\n\n");
-    printf("I am learning c.");
+    char *opretion;
+    int num1,num2;
+    opretion=argv[1];
+    num1 = atoi (argv[2]);
+    num2 = atoi (argv[3]);
+    printf("opretion is %d\n",opretion);
+    printf("Num1 is %d\n",num1);
+    printf("Num2 is %d\n",num2);
     'return0';
+
 }
+
