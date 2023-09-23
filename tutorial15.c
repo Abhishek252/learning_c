9c04b0c2dfbc6a8b17afcefe0a190d83711011d6
Himanshi <hmanshi332@gmail.com>
2023-09-23 22:48:29 +0530

    commit message

---
+++ tutorial15.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a;
+   int b;
+   printe ("Enter the first number:");
+   scanf("%d",&a);
+   printf("Enter the second number:");
+   scanf("%d",&b);
+   int c = a+b ;
+   printf("Sum of two number is is:%d",c);
+   getch();
+}
