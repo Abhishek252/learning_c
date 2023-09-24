5b4412662d8d05dc909c6ed20e44c179c1251d8c
Himanshi <hmanshi332@gmail.com>
2023-09-24 12:20:38 +0530

    commit message

---
+++ set_precision.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float myFloatNum = 3.5;
+
+    printf("%f/n",myFloatNum);
+    printf("%.1f/n",myFloatNum);
+    printf("%.2f/n",myFloatNum);
+    printf("%.4f",myFloatNum);
+    'return0';
+}
