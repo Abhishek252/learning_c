3ed71b93d6eb9f09411b6674222b046ba2b49e72
Himanshi <hmanshi332@gmail.com>
2023-09-24 10:51:43 +0530

    commit message

--- tutorial15.c
+++ tutorial15.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
-   int a;
-   int b;
-   printe ("Enter the first number:");
-   scanf("%d",&a);
-   printf("Enter the second number:");
-   scanf("%d",&b);
-   int c = a+b ;
-   printf("Sum of two number is is:%d",c);
-   getch();
+    //student data
+    int studentID = 15;
+    int studentAge = 23;
+    float studentFee = 75.9;
+    char studentGrade = 'D';
+
+     //printf variables
+     printf("student id:%d/n", studentID);
+     printf("studentage:%d/n", studentAge);
+     printf("studentfee:%f",  studentFee);
+     printf("student grade:%c", studentGrade);
+     'return0';
+
+
 }
