139af8b47845ac62fc26ac5993c0d698a8ff7cb4
Himanshi <hmanshi332@gmail.com>
2023-09-25 20:53:37 +0530

    printing string

---
+++ tutorial21.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char str[10] = {'B','H','O','P','A','L',};
+    int i;
+    for (i=0; str(i);i++);
+    printf("Lenth is %d",i);
+    'return0';
+}
---
+++ tutorial22.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char str[10]={'B','H','O','P','A','L'};
+    printf("%s",str);
+    'return0';
+}
