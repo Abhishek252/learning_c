b719e83f2e55ce7c43f2163afb8aaec8f444df5e
Himanshi <hmanshi332@gmail.com>
2023-09-26 14:03:44 +0530

    if else condistion

---
+++ tutorial25.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter a number");
+    scanf("%d",&x);
+    if (x%5)
+        printf( "Not Divisible by 5");
+    else
+        printf(" Divisible by 5");
+    'return0';
+}
