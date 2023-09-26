4255b957f951499c3e029ede07bd7d3c3a30c36c
Himanshi <hmanshi332@gmail.com>
2023-09-26 22:48:55 +0530

    do while loop

---
+++ tutorial29.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("Enter a number");
+    scanf("%d",&age);
+
+    switch(age);
+case 3:
+    printf("The age is 3");
+    break;
+case 13:
+    printf("The age is 13");
+    break;
+case 23:
+    printf("The age is 23");
+    break;
+default:
+    printf("Age is not 3,13 or 23");
+
+}
