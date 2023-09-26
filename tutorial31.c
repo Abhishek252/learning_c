0443e1d2571c0d836bcccd9b442656742d09bcb4
Himanshi <hmanshi332@gmail.com>
2023-09-26 23:00:38 +0530

    do while loop practice

---
+++ tutorial31.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter a number");
+    scanf("%d",&x);
+    if(x%3 == 0 && x%2 == 0)
+    printf("Divisible by 3&2");
+    else
+        printf("Not Divisible by 3&2");
+        getch();
+}
