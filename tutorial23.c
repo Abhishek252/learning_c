8a3b499e9d2870c146a5d0ca7f8aa531a0c879d4
Himanshi <hmanshi332@gmail.com>
2023-09-26 11:31:12 +0530

    sizeof operators practice

---
+++ tutorial23.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int myInt;
+
+
+    printf("%lu/n",sizeof(myInt));
+    'return0';
+}
---
+++ tutorial24.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int myInt;
+    float myFloat;
+    char myChar;
+    double myDouble;
+
+    printf("%lu/n",sizeof(myInt));
+    printf("%lu/n",sizeof(myFloat));
+    printf("%lu/n",sizeof(myChar));
+    printf("%lu/n",sizeof(myDouble));
+    'return0';
+}
