c8db94e7d26ad9b647b8d42719bf95dd51042296
Himanshi <hmanshi332@gmail.com>
2023-09-27 21:20:43 +0530

    switch stetment

---
+++ tutorial44.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int age,marks;
+        printf("Enter a number");
+        scnf("%d",&age);
+        printf("Enter a marks");
+        scanf("%d/n",&marks);
+
+        switch (age)
+    case 3:
+        printf("The age is a 3.");
+        switch (marks)
+    case 45:
+        printf ("The marks is 45.");
+    default:
+                printf("The marks is not 45.");
+}
