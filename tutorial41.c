78b788273dc2b9e26e46cad73017a3ecd88be229
Himanshi <hmanshi332@gmail.com>
2023-09-27 17:25:50 +0530

    Not,And operator

---
+++ tutorial41.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x = 12;
+    x!=6;
+    printf("%d\n",x);
+    'return0';
+}
---
+++ tutorial42.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int x=5;
+    x = 2;
+    printf("%d\n",x>2&& x<5);
+    'return0';
+
+}
