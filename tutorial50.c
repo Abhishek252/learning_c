266f7a5bc3d034efe167b0f9a6bd49720fc52409
Himanshi <hmanshi332@gmail.com>
2023-09-28 15:10:30 +0530

    switch practice

---
+++ tutorial50.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int age,marks;
+    printf("Enter a number");
+    scanf("%d/n",age,marks);
+
+    switch (age)
+    {
+    case 3:
+        printf("The age is 3.");
+        break;
+        switch(marks)
+    case 45:
+        print("The marks is 45.");
+        break;
+    }
+    'return0';
+}
