201fc0a48d8896d08e064df90c97aa6b5a877d02
Himanshi <hmanshi332@gmail.com>
2023-09-28 22:37:30 +0530

    break statement

---
+++ tutorial52.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    int i;
+    for(i=0;i<10; i++)
+    {
+       if(i == 4){
+        break;
+        printf("%d/n",i);
+       }
+
+    }
+    'return0';
+}
