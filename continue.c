06484af348ce4a0db23a9c6fc7c01aaf7655f139
Himanshi <hmanshi332@gmail.com>
2023-09-28 22:45:09 +0530

    continue statement

---
+++ continue.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for (i=0; i<10; i++){
+        if (i==4){
+            continue;
+            printf("%d/n",i5
+
+        }
+    }
+    'return0';
+}
