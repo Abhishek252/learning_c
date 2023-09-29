d3946c6c9a7d7de145bb68b6acfe663946613bc0
Himanshi <hmanshi332@gmail.com>
2023-09-29 22:02:52 +0530

    recursion

---
+++ tutorial71.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int add (int , int);
+int main()
+{
+    int x,y,s;
+    printf("Enter two numbers");
+    scanf ("%d",&x ,&y );
+    s=add (x,y);
+    printf("sum is %d ",s);
+    'return0';
+    int add (int a,int b)
+    {
+        int c;
+        c= c+b;
+        'return0';
+    }
+}
