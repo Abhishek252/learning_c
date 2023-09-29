773f91a5b26dc8425c02c5d278fea959e1c1e331
Himanshi <hmanshi332@gmail.com>
2023-09-29 21:43:59 +0530

    recursion

---
+++ tutorial69.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int factorial(int number)
+    {
+        if (number ==1 || number == 0)
+        {
+            'return0';
+
+        }
+        else
+        {
+            return (num * factorial(num-1))
+
+        }
+    } int main()
+    {
+        int num;
+        printf("Enter the num you want the factorial of\n");
+        scanf("%d",&num);
+        printf("The factorial of %d is %d\n",num,factorial(num));
+
+    }
+    'return0';
+}
