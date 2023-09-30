30d08174a3dcc5240557c1f29ebea25495378e91
Himanshi <hmanshi332@gmail.com>
2023-09-30 19:38:38 +0530

    sizeof string practice

---
+++ tutorial67.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char greetings[]= {'H','e','l','l','o', ' ', 'W','o','r','l','d','!','\0',};
+    char greetings2[]= "hello Worid!";
+    printf("%lu\n",sizeof(greetings));
+    printf("%lu\n",sizeof(greetings2));
+    'return0';
+}
