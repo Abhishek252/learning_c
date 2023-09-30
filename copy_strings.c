ecf08e8660fa16f25b3ce8d039ca39ccc49bf29c
Himanshi <hmanshi332@gmail.com>
2023-09-30 20:13:56 +0530

    Copy string practice

---
+++ copy_strings.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int  main()
+{
+    char str1[20]= "Good Evening!";
+    char str2[20];
+    //copy str1 to str2
+    strcpy(str2, str1);
+
+    //print str2
+    printf("%s",str1);
+
+    'return0';
+
+}
