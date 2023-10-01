07c1bdb9495240ad73670d71fbacd10a3dbddba8
Himanshi <hmanshi332@gmail.com>
2023-10-01 13:18:50 +0530

    pointers

---
+++ tutorial70.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    printf("Lets learn about pointers\n");
+    int  a= 76;
+    int *ptr = &a;
+    printf("The address of pointer to a is %p\n",&ptr);
+    printf("The address of a is %p\n",&a);
+N    printf("The address of a is %p\n",*ptra);
+    printf("The address of some garbage is %p\n",*ptr2);
+    printf("The value of a is %d\n",*ptra);
+    printf("The value of a %d\n",a);
+    'return0';
+}
--- tutorial71.c
+++ tutorial71.c
@@ -1,17 +1,5 @@
 #include<stdio.h>
-int add (int , int);
 int main()
 {
-    int x,y,s;
-    printf("Enter two numbers");
-    scanf ("%d",&x ,&y );
-    s=add (x,y);
-    printf("sum is %d ",s);
-    'return0';
-    int add (int a,int b)
-    {
-        int c;
-        c= c+b;
-        'return0';
-    }
+     printf("Lets learn about pointer to a is %p\n",&pointer 'a');
 }
