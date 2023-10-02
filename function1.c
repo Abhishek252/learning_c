78826be70cbc25b9db67bfa1ea9d8745ab463822
Himanshi <hmanshi332@gmail.com>
2023-10-02 12:09:43 +0530

    Function practice1,2

---
+++ function1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+  // create a Function
+    void myFunction()
+    {
+        printf("I just got executed!");
+
+    }
+    int main()
+    {
+        myFunction();  // call the Function
+
+    }
+    return0;
+
