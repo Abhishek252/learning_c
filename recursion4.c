f6641ba2602fd62d5473256e2d125941eee0aaf8
Himanshi <hmanshi332@gmail.com>
2023-10-03 11:17:53 +0530

    recurstion prectice3

---
+++ recursion4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int sum(int k);
+int main()
+{
+    int result = sum(10);
+    printf("%d",result);
+    'return0';
+
+}
+int sum(intk)
+{
+    if (K>0){
+        return k + sum (k -1);
+
+    }else
+    {
+        'return0';
+    }
+
+}
+
