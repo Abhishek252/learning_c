4ae6fda381a8c94e9f376a8adce36cd2d7987c3b
Himanshi <hmanshi332@gmail.com>
2023-10-04 19:35:47 +0530

    swaping practice2

---
+++ swaping.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/* function definition to swap the values */
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x; /* save the value at address x */
+    *x = *y ; /* put y into x */
+    *y = temp; /* put temp into y */
+    'return0';
+
+
+}
+int main()
+{
+    int a = 34, b = 74;
+    printf("%d and %d\n", a,b);
+    swap(&a,&b);
+    printf("%d and %d\n", a,b);
+    'return0';
+}
