b0854b4eab3c58da7989ce8c26f291c2fcc2fb32
Himanshi <hmanshi332@gmail.com>
2023-10-04 11:05:56 +0530

    math function Power, Square root ,Round number

---
+++ math.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("%f", sqrt(16));
+    'return0';
+
+}
---
+++ math1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("%f\n", ceil(1.4));
+    printf("%f\n", floor(1.4));
+    'return0';
+}
