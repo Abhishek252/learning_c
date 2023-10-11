fd2e73dca995d9615c9b9193807a524a64c219e2
Himanshi <hmanshi332@gmail.com>
2023-10-11 22:34:38 +0530

    app.c

---
+++ app.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float myFloat = 4.5;
+    printf("%f\n",myFloat);
+    printf("%.1f\n",myFloat);
+    printf("%.2f\n",myFloat);
+    printf("%.3f\n",myFloat);
+    printf("%.4f\n",myFloat);
+    printf("%.5f\n",myFloat);
+    return 0;
+}
