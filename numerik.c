85cf2c1a47c2d492afcc0a7038aa549577d17b31
Himanshi <hmanshi332@gmail.com>
2023-10-13 22:00:27 +0530

    numerik

---
+++ numerik.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int myNum = 45;
+    float myFloat =19.99;
+    char myChar='O';
+    printf("%d\n",myNum);
+    printf("%f\n",myFloat);
+    printf("%c\n",myChar);
+    return 0;
+}
