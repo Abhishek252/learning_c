efdab7eead558b264652f47fc05b2a8229421f75
Himanshi <hmanshi332@gmail.com>
2023-10-14 13:05:10 +0530

    Book.c, Day, Task

---
+++ Day.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+// Date data
+{
+
+    int date =14;
+    int month =10;
+    int year =2023;
+
+    //print variable
+    printf("Date : %d\n", date);
+    printf(" Month: %d\n", month);
+    printf(" Year: %d\n", year);
+    return 0;
+}
---
+++ Task.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+// Employee data
+{
+    int employeeId = 45;
+    float employeeSalary = 19.99;
+    char employeeName[20];
+
+    //print variable
+    printf("Employee id: %d\n", employeeId);
+    printf("Employee salary: %f\n", employeeSalary);
+    printf("Employee Name: %c\n", employeeName);
+    return 0;
+}
