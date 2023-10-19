addcdb6e5da7a795f2f519f05108484054c98c29
Himanshi <hmanshi332@gmail.com>
2023-10-19 22:29:56 +0530

    date1.c

---
+++ date1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int day = 2;
+    switch(day)
+    {
+    case 1:
+        printf("Monday");
+        break;
+    case 2:
+        printf("Sunday");
+        break;
+    }
+    return 0;
+}
