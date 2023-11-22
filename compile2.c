d1a2cdb1930ecd09307cb64c8f1c99c592eb9d80
Himanshi <hmanshi332@gmail.com>
2023-11-22 14:59:54 +0530

    practice

---
+++ compile2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i =10;
+    while(i>=1)
+    {
+        printf("%d\n",i*i);
+        i--;
+    }
+    return 0;
+}
+
+
---
+++ op2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter the value of x:");
+    scanf("%d",&x);
+    printf("Enter the value of y:");
+    scanf("%d",&y);
+    z= x-y;
+    printf("subtraction the value of x and y=%d",z);
+    getch();
+
+}
