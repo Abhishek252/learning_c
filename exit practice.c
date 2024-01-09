1cfcea90867d93e32e6b999911af8e075de98e46
Himanshi <hmanshi332@gmail.com>
2024-01-09 16:35:29 +0530

    function

---
+++ exit practice.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+    printf("start of the program\n");
+    printf("exiting the program\n");
+    getch();
+    exit(0);
+    printf("End of the program.\n");
+    return 0;
+    getch();
+}
---
+++ function 2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int add();
+int main()
+{
+    int a,b,c;
+    printf("Enter two number\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Addition is %d\n",c);
+
+}
+int add()
+{
+    int a,b,c;
+    c=a+b;
+    return 0;
+}
