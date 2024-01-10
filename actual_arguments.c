468f6d36fa0034e12ee537398eb826fe00082705
Himanshi <hmanshi332@gmail.com>
2024-01-10 16:12:49 +0530

    actual

---
+++ actual_arguments.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void sum(int i,int j,int k);
+int main()
+{
+    int a=5;
+    sun (3,2*a,a);
+
+}
+void sun (int i,int j, int k)
+{
+    int s;
+    s=i+j+k;
+    printf("sum is %d",s);
+
+}
