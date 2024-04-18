d6a2eba56cb79cda79867080254f164312581156
Himanshi <hmanshi332@gmail.com>
2024-04-19 00:03:45 +0530

    DSA

---
+++ array_insertion.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void display(int arr[], int n)
+{
+    for (int i= 0; i<n; i++)
+    {
+
+        printf("%d\n", arr[i]);
+    }
+    //printf("\n");
+}
+void indDelation(int arr[] , int size, int index)
+{
+    for (int i= index;  i< size - 1; i++)
+    {
+
+        arr[i] = arr[i+1];
+    }
+
+}
+int main()
+{
+    int arr[100] = {7, 8, 12, 27, 88};
+    int size = 5,  index = 0;
+    display(arr, size);
+    //indDeletion(arr, size, index);
+    size = size - 1;
+    display(arr, size);
+    return 0;
+}
+
